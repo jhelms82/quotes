6ee10ad41c10f7869310613c5b6c4be04e9c8976
James Helms <jhelms1014@gmail.com>
2021-05-10 13:54:39 -0500

    'd'

--- config/Database.php
+++ config/Database.php
@@ -1,11 +1,12 @@
 <?php
-class Database {
+class Database
+{
 //DB Params
 //private $host = 'localhost';
 //private $db_name = 'finalproject';
 //private $username = 'root';
 //private $password = 'sesame';
-private $conn;
+    private $conn;
 
 //public $host = 'frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 //public $db_name = 'd4plawwz86y7caqa';
@@ -13,29 +14,51 @@ private $conn;
 // public $password = 'eormls01njyhex6l';
 
 
-
 //DB connect
-public function connect () {
-    $url = getenv('JAWSDB_URL');
-    $dbparts = parse_url($url);
+    public function connect()
+    {
+//    $url = getenv('JAWSDB_URL');
+//    $dbparts = parse_url($url);
+//
+//    $hostname = $dbparts['host'];
+//    $username = $dbparts['user'];
+//    $password = $dbparts['pass'];
+//    $database = ltrim($dbparts['path'], '/');
+//
+//    $dsn = "mysql:host={$hostname}dbname={$database}";
+//    $this->conn = null;
+//
+//
+//    try {
+//        $this->conn = new PDO($dsn, $username, $password);
+////        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+////        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//}catch(PDOException $e) {
+//        echo 'Connection Error: ' . $e->getMessage();
+//}
+//
+//    return $this->conn;
+//}
 
-    $hostname = $dbparts['host'];
-    $username = $dbparts['user'];
-    $password = $dbparts['pass'];
-    $database = ltrim($dbparts['path'], '/');
 
-    $dsn = "mysql:host={$hostname}dbname={$database}";
-    $this->conn = null;
+        $dsn = 'ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        $username = 'ct6rmozpamywgnvd';
+        $password = 'vxv18vzsfkg4urzt';
 
 
-    try {
-        $this->conn = new PDO($dsn, $username, $password);
-//        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-//        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e) {
-        echo 'Connection Error: ' . $e->getMessage();
+        try {
+//        local development server connection
+//        if using a $password, add it as 3rd parameter
+            $db = new PDO($dsn, $username, $password);
+
+//         Heroku connection
+            $db = new PDO($dsn, $username, $password);
+        } catch (PDOException $e) {
+            $error = "Database Error: ";
+            $error .= $e -> getMessage ();
+            include ('view/error.php');
+            exit();
+        }
+    }
 }
 
-    return $this->conn;
-}
-}
