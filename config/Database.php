85a137e8d240009a46cd7d09517d6396099705d4
James Helms <jhelms1014@gmail.com>
2021-05-10 11:42:15 -0500

    second

--- config/Database.php
+++ config/Database.php
@@ -29,9 +29,9 @@ public function connect () {
 
 
     try {
-//        $this->conn = new PDO($dsn, $username, $password);
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->conn = new PDO($dsn, $username, $password);
+//        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+//        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
 }
