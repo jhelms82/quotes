c569f318b1d50142a60ee09ff818ac159f88c11f
James Helms <jhelms1014@gmail.com>
2021-05-10 22:47:41 -0500

    'd'

--- view/quote_list.php
+++ view/quote_list.php
@@ -81,14 +81,14 @@
                         <tbody>
                         <?php foreach ($quotes as $quote) : ?>
                             <tr>
-                                <td scope="row"><?= $quote['quote']; ?></td>
+                                <td><?= $quote['quote']; ?></td>
                                 <?php if ($quote['author_name']) { ?>
-                                    <td scope="row"><?= $quote['author_name']; ?></td>
+                                    <td><?= $quote['author_name']; ?></td>
                                 <?php } else { ?>
                                     <td>None</td>
                                 <?php } ?>
                                 <?php if ($quote['category_name']) { ?>
-                                    <td scope="row"><?= $quote['category_name']; ?></td>
+                                    <td><?= $quote['category_name']; ?></td>
                                 <?php } else { ?>
                                     <td>None</td>
                                 <?php } ?>
