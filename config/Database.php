739ad7ea3b5216ed6f2d24eb5b403835ae4283d4
James Helms <jhelms1014@gmail.com>
2021-05-10 21:32:49 -0500

    'fixed'

--- config/Database.php
+++ config/Database.php
@@ -11,20 +11,28 @@ class Database
 
 
 
+
+
 //DB connect
     public function connect()
     {
-        $url = getenv('JAWSDB_MARIA_URL');
-        $dbparts = parse_url($url);
+//        $url = getenv('JAWSDB_MARIA_URL');
+//        $dbparts = parse_url($url);
+//
+//        $hostname = $dbparts['host'];
+//        $username = $dbparts['user'];
+//        $password = $dbparts['pass'];
+//        $database = ltrim($dbparts['path'], '/');
+//
+//        $dsn = "mysql:host={$hostname}dbname={$database}";
+//        $this->conn = null;
 
-        $hostname = $dbparts['host'];
-        $username = $dbparts['user'];
-        $password = $dbparts['pass'];
-        $database = ltrim($dbparts['path'], '/');
-
-        $dsn = "mysql:host={$hostname}dbname={$database}";
-        $this->conn = null;
-
+        $host = 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        $db = 'scyddzzkvdymrsrw';
+        $username = 'r8kel2jekjdp3v9p';
+        $password = 'w798oejyiro3ditm';
+        $charset = 'utmf8md4';
+        $dsn = 'mysql:host=$host;dbname=$db;charset=$charset';
 
         try {
             $this->conn = new PDO($dsn, $username, $password);
